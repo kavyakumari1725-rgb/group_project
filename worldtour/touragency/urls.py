986b7a5580b28f91426c33e4c0b2c8a7c5685d20
Azmat <azmatullahrana552@gmail.com>
2025-10-15 12:51:01 +0100

    added

--- worldtour/touragency/urls.py
+++ worldtour/touragency/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     path('',views.index,name='index'),
+    path('about',views.about_us,name='about_us'),
 ]
--- worldtour/touragency/views.py
+++ worldtour/touragency/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(response):
     return render(response,'index.html')
+def about_us(response):
+    return render(response,'about_us.html')
