d469e590f91235534bf6125cf8b0fcb00a447d17
kavyakumari1725 <kavyakumari1725@gmail.com>
2025-10-15 12:23:40 +0100

    add destination

--- worldtour/touragency/views.py
+++ worldtour/touragency/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from. models import Tour
+
 
 # Create your views here.
 def index(response):
-    return render(response,'index.html')
+    return render(response,'index.html',)
+def destination(response):
+    tours = Tour.objects.all()
+    context = {'tours': tours}
+    return render(response,'destination.html',context)
