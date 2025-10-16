c3f47c2fdabad5f070c82d76143aad2c0a4ccf94
kavyakumari1725 <kavyakumari1725@gmail.com>
2025-10-16 11:48:47 +0100

    added

--- worldtour/touragency/views.py
+++ worldtour/touragency/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(response):
     return render(response,'index.html')
+def packages(response):
+    return render(response,'packages.html')
