d64ad603b1fd05ee8cc34c1011418beb6380752d
kavyakumari1725-rgb <kavyakumari1725@gmail.com>
2025-10-21 20:29:46 +0100

    changes

--- worldtour/touragency/models.py
+++ worldtour/touragency/models.py
@@ -7,7 +7,8 @@ class Tour(models.Model):
     destination_country=models.CharField(max_length=64)
     no_of_days=models.IntegerField()
     price=models.IntegerField()
+    image = models.ImageField(upload_to='tours/', blank=True, null=True)
 
 
 def __str__(self):
-    return (f"ID:{self.id}:from {self.original_country} to {self.destination_country}, {self.no_of_days} costs {self.price}")
+    return f"ID:{self.id}:from {self.original_country} to {self.destination_country}, {self.no_of_days} costs {self.price}"
